585488b62a429d3c588c48636a96f4307e8a1ffb
Mumpet <memunazakari188@gmail.com>
2023-04-20 22:47:21 -0700

    Puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * return: always 0 (success)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return(0);
+}
