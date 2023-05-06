79aa89aed4a728edb903f9b0b5bc030047747483
Mumpet <memunazakari188@gmail.com>
2023-05-06 03:15:28 -0700

    main.h

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -9,11 +9,11 @@ int _strcmp(char *s1, char *s2);
 void reverse_array(int *a, int n);
 char *string_toupper(char *);
 char *cap_string(char *);
-char *heet(char *);
+char *leet(char *);
 void print_number(int n);
 char *rot13(char *);
 char *infinite_add(char *n1, char *n2, char *r, int size_r);
 void print_buffer(char *b, int size);
 char add_strings(char *n1, char *n2, char *r, int r_index);
 
-#endif /* MAIN_H */
+#endif
