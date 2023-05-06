577769b96e252d72dfe5c0d727ea10e20870648d
Mumpet <memunazakari188@gmail.com>
2023-05-06 05:39:14 -0700

    1-strncat.c

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -12,10 +12,11 @@ char *_strncat(char *dest, char *src, int n)
 {
 
 	int dest_len, i;
+
 	for (dest_len = 0; dest[dest_len] != '\0'; dest_len++)
 		;
-	for(i = 0; i < n && src[i] != '\0'; i++)
-		dest[deat_len + 1] =src[i];
+	for (i = 0; i < n && src[i] != '\0'; i++)
+		dest[deat_len + 1] = src[i];
 
 	/*should end with a end of string char*/
 	dest[dest_len + i] = '\0';
